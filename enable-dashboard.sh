535b68059cd5420f980806a921defe1de28e2e5c
tarilabs <matteo.mortari@gmail.com>
2024-10-26 16:58:26 +0200

    wip
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- enable-dashboard.sh
+++ enable-dashboard.sh
@@ -9,6 +9,10 @@ for DEPLOYMENT in $DEPLOYMENTS; do # bash separation
   kubectl wait --for=condition=available --timeout=2m deployment/$DEPLOYMENT -n kubernetes-dashboard
 done
 
+echo
+echo "instructions above ^^^"
+echo
+
 curl -s "https://raw.githubusercontent.com/tarilabs/k8s-dashboard/main/serviceAccount.yaml" | kubectl apply -f -
 sleep 2
 curl -s "https://raw.githubusercontent.com/tarilabs/k8s-dashboard/main/clusterRoleBinding.yaml" | kubectl apply -f -
@@ -19,5 +23,9 @@ sleep 3
 echo "Bearer token:"
 kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
 echo ""
+
+echo
+echo "about to port-fwd to http://localhost:8443"
+echo
 
 kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
