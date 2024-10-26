e62f86200fa00ab3aa27bfcd359c9c3ad86b0105
tarilabs <matteo.mortari@gmail.com>
2024-10-26 16:59:53 +0200

    wip
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- enable-dashboard.sh
+++ enable-dashboard.sh
@@ -3,15 +3,15 @@
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
 
+echo
+echo "instructions above ^^^"
+echo
+
 echo "Waiting for all Deployments in kubernetes-dashboard..."
 DEPLOYMENTS=$(kubectl get deployments -n kubernetes-dashboard -o jsonpath='{.items[*].metadata.name}')
 for DEPLOYMENT in $DEPLOYMENTS; do # bash separation
   kubectl wait --for=condition=available --timeout=2m deployment/$DEPLOYMENT -n kubernetes-dashboard
 done
-
-echo
-echo "instructions above ^^^"
-echo
 
 curl -s "https://raw.githubusercontent.com/tarilabs/k8s-dashboard/main/serviceAccount.yaml" | kubectl apply -f -
 sleep 2
@@ -25,7 +25,7 @@ kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"
 echo ""
 
 echo
-echo "about to port-fwd to http://localhost:8443"
+echo "about to port-fwd to https://localhost:8443"
 echo
 
 kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
