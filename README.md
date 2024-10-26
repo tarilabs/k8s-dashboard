4b49839880901472d3a44bceddf86fd538526aed
tarilabs <matteo.mortari@gmail.com>
2024-10-26 16:42:31 +0200

    initial commit
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm so tired of doing this all over again, automating :)
+
+curl -s "https://raw.githubusercontent.com/tarilabs/k8s-dashboard/main/enable-dashboard.sh"  | bash
---
+++ enable-dashboard.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+echo "Waiting for all Deployments in kubernetes-dashboard..."
+DEPLOYMENTS=$(kubectl get deployments -n kubernetes-dashboard -o jsonpath='{.items[*].metadata.name}')
+for DEPLOYMENT in $DEPLOYMENTS; do # bash separation
+  kubectl wait --for=condition=available --timeout=2m deployment/$DEPLOYMENT -n kubernetes-dashboard
+done
+
+kubectl apply -f github.com/tarilabs/k8s-dashboard/serviceAccount.yaml
+sleep 2
+kubectl apply -f github.com/tarilabs/k8s-dashboard/clusterRoleBinding.yaml
+sleep 2
+kubectl apply -f github.com/tarilabs/k8s-dashboard/bearerTokenSecret.yaml
+sleep 3
+
+echo "Bearer token:"
+kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
+echo ""
+
+kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443
